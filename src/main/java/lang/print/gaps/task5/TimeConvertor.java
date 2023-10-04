0382bd7d87d4fa01499c7f03c7bb2e9ee5625e79
Philip <okruzhko.f@gmail.com>
2023-10-04 22:39:24 +0400

    Complete s0m3t5

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    public static void main(String[] args) {
+        TimeConvertor tc = new TimeConvertor();
+        tc.convert(20.5f);
+    }
     public void convert(float minutes) {
+        System.out.println((float) minutes*60);
     }
 }
